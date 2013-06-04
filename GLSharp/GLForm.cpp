b43d7b4f617a4ab64f2b416e9c83ae7eef86c54f
Darren Horrocks <killallthehumans@gmail.com>
2013-06-04 13:55:44 -0700

    format fix

--- GLSharp/GLForm.cpp
+++ GLSharp/GLForm.cpp
@@ -1,13 +1,13 @@
 #include "stdafx.h"
 #include "GLForm.h"
-  
+
 System::Windows::Forms::GLForm::GLForm()
 	: Form()
 {
 	m_gl = gcnew System::Drawing::GLSharp::GL();
-  m_wgl = gcnew System::Drawing::GLSharp::WGL();
+	m_wgl = gcnew System::Drawing::GLSharp::WGL();
 
-  this->Load += gcnew EventHandler(this, &GLForm::GL_Load);
+	this->Load += gcnew EventHandler(this, &GLForm::GL_Load);
 }
 
 System::Void System::Windows::Forms::GLForm::GL_Load(System::Object ^sender, System::EventArgs ^e)
